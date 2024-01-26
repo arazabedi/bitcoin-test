c896572538bc972ea069c45abb3118176a139313
Araz <arazabedi@hotmail.com>
2024-01-26 21:57:00 +0200

    responsiveness fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={`${inter.className}`}>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
-					// enableSystem
-					// disableTransitionOnChange
+				// enableSystem
+				// disableTransitionOnChange
 				>
 					{children}
 				</ThemeProvider>
